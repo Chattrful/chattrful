ea6cbfc1db982b461eb6f585e7ccf36d164f2363
Sean Yeoh <sean.yeoh@hotmail.com>
2020-09-07 10:52:39 +0800

    finish js

--- app/controllers/metronic_controller.rb
+++ app/controllers/metronic_controller.rb
@@ -1,4 +1,5 @@
 class MetronicController < ActionController::Base
   def show
+    @kt_aside_toggle_state = cookies[:kt_aside_toggle_state]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resource :metronic, controller: 'metronic', only: :show
-
-  root to: 'home#index'
+  if Rails.env.development?
+    resource :metronic, controller: 'metronic', only: :show
+  end
 end
