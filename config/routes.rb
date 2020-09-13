21d05c6b42d69ccfb80ca0563630dc55abe7ec70
Sean Yeoh <sean.yeoh@hotmail.com>
2020-09-13 17:11:56 +0800

    rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  if Rails.env.development?
-    resource :metronic, controller: 'metronic', only: :show
-  end
+  resource :metronic, controller: 'metronic', only: :show
 end
---
+++ spec/system/metronic_spec.rb
@@ -0,0 +1,5 @@
+RSpec.feature "Authentication", type: :system, js: true do
+  it 'can visit metronic' do
+    visit metronic_path
+  end
+end
