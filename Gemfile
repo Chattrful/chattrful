b3263bd9d5936c8f99688fcffe601e47ff12dbfc
Sean Yeoh <sean.yeoh@hotmail.com>
2020-10-13 13:26:36 +0800

    Sanitize

--- Gemfile
+++ Gemfile
@@ -69,4 +69,5 @@ gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
 gem "anycable-rails", "~> 1.0"
 gem "bundler-audit"
 gem "kaminari"
+gem "rinku"
 gem "sidekiq"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def markup(text)
+    result = h(text)
+    result = Rinku.auto_link(result, :all, 'target="_blank"')
+    result.html_safe
+  end
 end
