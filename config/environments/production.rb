0fe3113abe8e3e8c9406d9b653f13cb414a31cd6
Sean Yeoh <sean.yeoh@hotmail.com>
2020-10-14 13:02:34 +0800

    Rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "chattr_production"
+  # config.active_job.queue_name_prefix = "chattrful_production"
 
   config.action_mailer.perform_caching = false
 
