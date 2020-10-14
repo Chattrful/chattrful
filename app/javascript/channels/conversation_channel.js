b8bd9f96ef0bc09ed120506d365ef0dfc645ae1c
Sean Yeoh <sean.yeoh@hotmail.com>
2020-10-14 22:47:30 +0800

    move scss to vendor

--- app/javascript/channels/conversation_channel.js
+++ app/javascript/channels/conversation_channel.js
@@ -5,32 +5,34 @@ import ScrollToBottom from '../util/scroll_to_bottom'
 document.addEventListener('turbolinks:load', () => {
   const chatMessages = document.querySelector('.js-chat-messages')
 
-  consumer.subscriptions.create(
-    {
-      channel: 'ConversationChannel',
-      conversation_id: document.querySelector('[data-conversation-id]').dataset.conversationId
-    },
-    {
-      connected () {
-        // Called when the subscription is ready for use on the server
-        console.log('connected to ConversationChannel')
+  if (chatMessages) {
+    consumer.subscriptions.create(
+      {
+        channel: 'ConversationChannel',
+        conversation_id: document.querySelector('[data-conversation-id]').dataset.conversationId
       },
+      {
+        connected () {
+          // Called when the subscription is ready for use on the server
+          console.log('connected to ConversationChannel')
+        },
 
-      disconnected () {
-        // Called when the subscription has been terminated by the server
-        console.log('disconnected to ConversationChannel')
-      },
+        disconnected () {
+          // Called when the subscription has been terminated by the server
+          console.log('disconnected to ConversationChannel')
+        },
 
-      received (data) {
-        const appendedMessage = document.querySelector(`[data-message-id="${data.message_id}"]`)
+        received (data) {
+          const appendedMessage = document.querySelector(`[data-message-id="${data.message_id}"]`)
 
-        if (!appendedMessage) {
-          const tempHTML = document.createElement('div')
-          tempHTML.innerHTML = data.html
-          chatMessages.append(tempHTML.firstElementChild)
-          ScrollToBottom({element: chatMessages})
+          if (!appendedMessage) {
+            const tempHTML = document.createElement('div')
+            tempHTML.innerHTML = data.html
+            chatMessages.append(tempHTML.firstElementChild)
+            ScrollToBottom({element: chatMessages})
+          }
         }
       }
-    }
-  )
+    )
+  }
 })
