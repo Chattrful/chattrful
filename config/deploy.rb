3a5799cef32ec7b02af667cd12b293e0040afdcb
Sean Yeoh <sean.yeoh@hotmail.com>
2020-10-23 15:23:48 +0800

    rbenv rehash

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,7 +89,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+      invoke 'rbenv rehash'
     end
   end
 
