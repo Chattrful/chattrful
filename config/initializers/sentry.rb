9f99b4d553620a0f55f75275d54cf622798560d8
Sean Yeoh <sean.yeoh@hotmail.com>
2020-10-24 23:54:11 +0800

    sentry production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,7 +5,7 @@ Raven.configure do |config|
 
   config.breadcrumbs_logger = [:sentry_logger, :active_support_logger]
   config.dsn = "https://a1e56d8f699842e5ab86393c5562de43@o462444.ingest.sentry.io/5465856"
-  # config.environments = %w[ production ]
+  config.environments = %w[production]
   config.processors -= [Raven::Processor::PostData]
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
