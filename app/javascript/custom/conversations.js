53ecda1bd39a6d9138d9cbfcfcec01d9586333a1
Sean Yeoh <sean.yeoh@hotmail.com>
2020-11-04 20:35:20 +0800

    use offcanvas instead of slide in

--- app/javascript/custom/conversations.js
+++ app/javascript/custom/conversations.js
@@ -6,21 +6,25 @@ document.addEventListener("turbolinks:load", () => {
 
   conversationLists.forEach(conversationList => {
     conversationList.addEventListener('click', event => {
-      document.querySelectorAll('.conversation-list__item').forEach(conversationList => {
+      document.querySelectorAll('.conversation-list__item--active').forEach(conversationList => {
         conversationList.classList.remove('conversation-list__item--active');
       })
 
       event.currentTarget.classList.add('conversation-list__item--active')
 
       if (Breakpoints.isMobileOrTablet()) {
-        mobileConversationChatWidget.classList.add('mobile-conversation-chat-widget--open')
+        mobileConversationChatWidget.classList.add('offcanvas-mobile-on')
       }
     })
   });
 
   if (Breakpoints.isMobileOrTablet()) {
     $(document).on('click', '.js-mobile-close-chat-widget', () => {
-      mobileConversationChatWidget.classList.remove('mobile-conversation-chat-widget--open')
+      document.querySelectorAll('.conversation-list__item--active').forEach(conversationList => {
+        conversationList.classList.remove('conversation-list__item--active');
+      })
+
+      mobileConversationChatWidget.classList.remove('offcanvas-mobile-on')
     })
   }
 })
